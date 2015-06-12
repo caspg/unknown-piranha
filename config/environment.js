17b69fd2e1f8e5181d3970144030dc1069e6e7c8
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-12 22:45:46 +0200

    setup for using pods structure

--- config/environment.js
+++ config/environment.js
@@ -3,6 +3,7 @@
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'unknown-piranha',
+    podModulePrefix: 'pod-example/pods',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
