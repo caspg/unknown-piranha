7cb3a3e60a9253f5feb8317ff6e051422942a15c
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-13 21:55:30 +0200

    create saveUser action

--- app/pods/users/sign-up/route.js
+++ app/pods/users/sign-up/route.js
@@ -1,7 +1,26 @@
+import Firebase from 'firebase';
+import config from '../../../config/environment';
 import Ember from 'ember';
 
 export default Ember.Route.extend({
   model: function() {
     return this.store.createRecord('user');
+  },
+
+  actions: {
+    saveUser: function() {
+      var model = this.modelFor(this.routeName);
+      var ref = new Firebase(config.firebase);
+      ref.createUser({
+        email: model.get('email'),
+        password: model.get('password')
+      }, function(error, userData) {
+        if (error) {
+          console.log('Error createing user:', error);
+        } else {
+          console.log('Successfully created user account with uid:', userData.uid);
+        }
+      });
+    }
   }
 });
