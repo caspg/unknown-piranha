81ceadc7ae94b1bb31c5a204d4cd48cdb5cd1b84
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-13 19:30:17 +0200

    add validation for user

--- app/pods/user/model.js
+++ app/pods/user/model.js
@@ -1,6 +1,21 @@
 import DS from 'ember-data';
+import EmberValidations from 'ember-validations';
 
-export default DS.Model.extend({
+export default DS.Model.extend(EmberValidations.Mixin, {
   email: DS.attr('string'),
-  password: DS.attr('string')
+  password: DS.attr('string'),
+
+  validations: {
+    email: {
+      presence: true,
+      format: {
+        with: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+        message: 'your email is invalid'
+      }
+    },
+    password: {
+      presence: true,
+      length: { minimum: 8 }
+    }
+  }
 });
