e16dd1d29b2cd7e279f245a5d057cfc2b3da093f
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-13 21:55:04 +0200

    disable CSP

--- config/environment.js
+++ config/environment.js
@@ -5,7 +5,6 @@ module.exports = function(environment) {
     modulePrefix: 'unknown-piranha',
     podModulePrefix: 'unknown-piranha/pods',
     environment: environment,
-    contentSecurityPolicy: { 'connect-src': "'self' wss://*.firebaseio.com" },
     firebase: 'https://unknown-piranha.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
