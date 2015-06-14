0c19a9e6df73c06838993abdabeaaea3bf4bebde
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-14 22:19:43 +0200

    update place model

--- app/pods/place/model.js
+++ app/pods/place/model.js
@@ -1,5 +1,14 @@
 import DS from 'ember-data';
+import EmberValidations from 'ember-validations';
 
 export default DS.Model.extend({
-  
+  name: DS.attr('string'),
+  address: DS.attr('string'),
+  description: DS.attr('string'),
+
+  validations: {
+    name: { presence: true },
+    address: { presence: true },
+    description: { presence: true }
+  }
 });
