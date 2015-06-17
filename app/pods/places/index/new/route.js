73c747d680d3db93a43ab3a8d9fb16f07ace7a6e
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-17 21:38:03 +0200

    update places.index.new route and template

--- app/pods/places/index/new/route.js
+++ app/pods/places/index/new/route.js
@@ -8,7 +8,21 @@ export default Ember.Route.extend({
   actions: {
     savePlace: function() {
       var model = this.modelFor(this.routeName);
-      model.save();
+      model.save().then(() => {
+        this.transitionTo('places.index');
+      });
+    }
+  },
+
+  activate: function() {
+    this.controllerFor('places.index').set('isNewForm', true);
+  },
+  deactivate: function() {
+    this.controllerFor('places.index').set('isNewForm', false);
+
+    var model = this.modelFor('places.index.new');
+    if (model.get('isNew')) {
+      model.destroyRecord();
     }
   }
 });
