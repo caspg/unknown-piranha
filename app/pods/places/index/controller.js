bac66ee6c12e2c0bfabf5c3db846234fab569dee
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-18 21:03:27 +0200

    add hideButton property

--- app/pods/places/index/controller.js
+++ app/pods/places/index/controller.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  isNewForm: false
+  isNotAuthenticated: Ember.computed.not('session.isAuthenticated'),
+  isNewForm: false,
+  hideButton: Ember.computed.or('isNewForm', 'isNotAuthenticated')
 });
