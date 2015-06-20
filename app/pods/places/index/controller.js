9836af3620de8102506efa91e943cebc39e1bc7c
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-20 18:37:25 +0200

    use google-map component

--- app/pods/places/index/controller.js
+++ app/pods/places/index/controller.js
@@ -2,4 +2,7 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isNotAuthenticated: Ember.computed.not('session.isAuthenticated'),
+  zoom: 14,
+  centerLat: 52.2333,
+  centerLng: 21.0167
 });
