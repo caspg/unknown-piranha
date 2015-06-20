a9431813335b6b08c2d089f884e757a2bfc55ebd
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-20 20:49:32 +0200

    update transition.js

--- app/transitions.js
+++ app/transitions.js
@@ -1,6 +1,6 @@
 export default function(){
   this.transition(
-    this.fromRoute('places.index.index'),
+    this.fromRoute(null),
     this.toRoute('places.index.new'),
     this.use('toRight'),
     this.reverse('toLeft')
