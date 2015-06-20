4ca7edefe40d38f494af738fc2525d65de0cb0ac
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-20 20:49:58 +0200

    create picker-marker for adding new place

--- app/pods/places/index/controller.js
+++ app/pods/places/index/controller.js
@@ -1,8 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  isNewForm: false,
   isNotAuthenticated: Ember.computed.not('session.isAuthenticated'),
   zoom: 14,
   centerLat: 52.2333,
-  centerLng: 21.0167
+  centerLng: 21.0167,
+
+
+  markers: Ember.computed("isNewForm", function() {
+    if (this.get("isNewForm")) {
+      return this.controllerFor('places.index.new').get('picker');
+    }
+  })
 });
--- app/pods/places/index/new/controller.js
+++ app/pods/places/index/new/controller.js
@@ -1,4 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  picker: Ember.computed(function() {
+    return Ember.A([{ lat: 52.2333, lng: 21.0167, isDraggable: true, isInfoWindowVisible: true }]);
+  })
 });
