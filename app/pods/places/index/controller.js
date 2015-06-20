8ae9ca6785d3708f360dba6b88b19ab207eb5467
Kacper Goliński <kacper.golinski@netguru.co>
2015-06-20 17:55:20 +0200

    style new-place pannel

--- app/pods/places/index/controller.js
+++ app/pods/places/index/controller.js
@@ -2,6 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   isNotAuthenticated: Ember.computed.not('session.isAuthenticated'),
-  isNewForm: false,
-  hideButton: Ember.computed.or('isNewForm', 'isNotAuthenticated')
 });
